7fdc4ba9633d0f98d47d9d1a7cf570897c38b14c
Michael Tauberg <mtauberg@fb.com>
2021-01-28 16:05:10 -0800

    uploaded files and collateral

---
+++ add_polarity_score.py
@@ -0,0 +1,320 @@
+import pandas as pd 
+
+media_polarity = {
+        "247sports.com": "NA",
+        "ABC News": -1,
+        "AirforceTimes": "NA",
+        "Alternet": -2,
+        "American Free Press": "NA",
+        "American Spectator": "NA",
+        "amp.freep.com": "NA",
+        "AOL News": "NA",
+        "aol.sportingnews.com": "NA",
+        "Arkansas Democrat-Gazette": -2,
+        "ArmyTimes": "NA",
+        "arstechnica.com": "NA",
+        "Atlanta Journal-Constitution": -1,
+        "Atlantic": -1,
+        "augustachronicle.com": "NA",
+        "AZCentral": 0,
+        "Baltimore Sun": "NA",
+        "Benzinga": "NA",
+        "berkshireeagle.com": "NA",
+        "Billboard": "NA",
+        "Blaze": 2,
+        "Bleacher Report": "NA",
+        "blogs.barrons.com": "NA",
+        "blogs.indystar.com": 0,
+        "Bloomberg": -1,
+        "Breitbart": 2,
+        "Business Insider": 0,
+        "BusinessWeek.com": "NA",
+        "Bustle": -1,
+        "Buzzfeed": -2,
+        "CBS News": -1,
+        "cbssports.com": -1,
+        "Central Maine News": "NA",
+        "cherokeephoenix.org": "NA",
+        "Chicago Tribune": 0,
+        "Christian Science Monitor": 0,
+        "cincinnati.com": "NA",
+        "cleveland.com": "NA",
+        "CNBC": "NA",
+        "CNET": 0,
+        "CNN": -2,
+        "CNN U.S. Edition": -2,
+        "CNS News": 2,
+        "colorlines.com": "NA",
+        "Columbus Dispatch": "NA",
+        "Conversation": "NA",
+        "creators.com": "NA",
+        "CSPAN": "NA",
+        "Daily Beast": -2,
+        "Daily Caller": 2,
+        "Daily Dot": -1,
+        "Daily Kos": -2,
+        "Daily Signal": 2,
+        "Daily Source": "NA",
+        "Dallas Morning News": 0,
+    "Deadspin": "NA",
+    "E! Online": "NA", 
+    "eater.com": "NA",
+    "El Nuevo Dia": "NA",
+        "elitedaily.com": "NA",
+    "engadget": "NA",
+    "Entertainment Weekly": "NA",
+    "ESPN": "NA",
+    "eurweb": "NA",
+    "FactCheck": 0,
+    "Fansided": "NA",
+    "fark.com": "NA",
+    "fayobserver.com": "NA",
+    "FiveThirtyEight": 0,
+    "Forbes": 0,
+    "Fortune": 0,
+    "Fox News": 2,
+    "Fox Sports": "NA",
+    "Fusion": "NA",
+    "Gawker": "NA",
+    "gazettenet.com": "NA",
+    "Gizmodo": -1,
+    "globalpost.com": "NA",
+    "gq.com": -1,
+    "Grist": -1,
+    "Guardian": -1,
+    "Guardian US": -1,
+    "Harper's Magazine": "NA",
+    "Hartford Courant": "NA",
+    "hollywoodlife.com": "NA",
+    "HonululuAdvertiser": "NA",
+    "Hot Air": 1,
+    "houstonchronicle": 0,
+    "Hoy Chicago": "NA",
+    "Huffington Post - United States": -2,
+    "Huffington Post Black Voices": -2,
+    "HuffPost": -2,
+    "HuffPost Latino Voices": -2,
+    "IBTimes": 0,
+    "ign.com": "NA",
+    "ihavenet.com": "NA",
+    "Indian Country Today": "NA",
+    "Indiewire": "NA",
+    "Inquirer": "NA",
+    "Inquisitr": "NA",
+    "Intercept": -2,
+    "Investors": "NA",
+    "Jezebel": -2,
+    "LA Times": -1,
+    "livescience.com": 0,
+    "marinetimes.com": "NA",
+    "Marketwatch": 0,
+    "Mashable!": "NA",
+    "media.oregonlive.com": "NA",
+    "Miami Herald": -1,
+    "Mic": "NA",
+    "Milwaukee Journal Sentinel": "NA",
+    "MintPress News": "NA",
+    "mlive.com": "NA",
+    "Mongabay": "NA",
+    "monstersandcritics": "NA",
+    "Mother Jones": -2,
+    "mysanantonio.com": "NA",
+    "National Catholic Register": "NA",
+    "National Memo": "NA",
+    "National Review": 2,
+    "NavyTimes": "NA",
+    "NBC Breaking News": -1,
+    "ncronline.org": "NA",
+    "New York Post": 1,
+    "New York Times": -1,
+    "New Yorker": -2,
+    "newsblaze.com": "NA",
+    "NewsBusters": 2,
+    "Newsday": "NA",
+    "Newsmax": 1,
+    "newsone.com": -2,
+    "Newsweek": 0,
+    "newsy": 0,
+    "nhgazette.com": "NA",
+    "nj.com": "NA",
+    "norwichbulletin.com": "NA",
+    "NPR": -1,
+    "Oan": 2,
+    "observer.com": 0,
+    "Omaha World-Herald": "NA",
+    "opednews.com": "NA",
+    "OpenSecrets": 0,
+    "opposingviews.com": "NA",
+    "Orange County Register": 1,
+    "Orlando Sentinel": "NA",
+    "Parade": "NA",
+    "patriot post": "NA",
+    "PBS NewsHour": 0,
+    "PEOPLE.com": "NA",
+    "pilotonline.com": "NA",
+    "pitchfork.com": "NA",
+    "Pittsburgh Post-Gazette": 1,
+    "PJ Media": 2,
+    "Politico": -1,
+    "polygon.com": "NA",
+    "popmatters": "NA",
+    "Post and Courier": "NA",
+    "Press-Republican": "NA",
+    "ProPublica": -1,
+    "providencejournal.com": "NA",
+    "Quartz": 0,
+    "Raw Story": -2,
+    "Re/code": "NA",
+    "RealClearPolitics": 0,
+    "Reason.com": 1,
+    "Red Herring": "NA",
+    "RedState": 2,
+    "Refinery29": -2,
+    "Reuters": 0,
+    "Roll Call": 0,
+    "Rolling Stone": -2,
+    "rttnews": "NA",
+    "rutlandherald.com": "NA",
+    "Salon": -2,
+    "San Diego Union Tribune": -1,
+    "San Jose Mercury News": "NA",
+    "Saturday Evening Post": "NA",
+    "SB Nation": "NA",
+    "Schwartzreport": "NA",
+    "Scientific American": -1,
+    "Seattle Times": "NA",
+    "sentinelsource.com": "NA",
+    "SF Chronicle": -1,
+    "Slate.com": -2,
+    "South Florida Sun-Sentinel": "NA",
+    "St. Louis Post-Dispatch": "NA",
+    "St.Paul Pioneer-Press": "NA",
+    "Star Tribune": "NA",
+    "Stars and Stripes" :"NA",
+    "Stateline - News about state government and politics from The Pew Center on the States": "NA",
+    "Superglue: MSNBC": -2,
+    "Taegan Goddard's Political Wire": "NA",
+    "Talk Media News": "NA", 
+    "Talking Points Memo": "NA",
+    "Tampa Bay Times": "NA",
+    "TechCrunch": "NA",
+    "techradar.com": "NA",
+    "The Boston Globe": -1,
+    "The Buffalo News": "NA",
+    "The Insider": "NA",
+    "The Nation": -2,
+    "The New Replublic": -2,
+    "The Onion": -1,
+    "The Street": "NA",
+    "The Week": 0,
+    "The Wrap": "NA",
+    "TheRealNews.com": "NA",
+    "theroot.com": -1,
+    "thrillist.com": "NA",
+    "Time": -1,
+    "TMZ": "NA",
+    "townhall.com": 2,
+    "Truthout": -1,
+    "UPI": "NA",
+    "uproxx.com": "NA",
+    "Upworthy": "NA",
+    "US News & World Report": -1,
+    "US Weekly": "NA",
+    "USA Today": 0,
+    "Vanity Fair": -1,
+    "variety.com": 0,
+    "Verge": -1,
+    "VICE": -2,
+    "Vox": -2,
+    "Wall Street Journal": 1,
+    "Washington Post": -1,
+    "wired": 0,
+    "x17online.com": "NA",
+    "yardbarker": "NA",
+    "zdnet": "NA",
+}
+
+def f(row):
+    if row['media_name'] in media_polarity.keys():
+        return media_polarity[row['media_name']]
+    else:
+        return "NA"
+
+protest_data = pd.read_csv("protest-all-story-urls-20210117023531.csv")
+protest_data["polarity"] = protest_data.apply(f, axis=1)
+
+riot_data = pd.read_csv("riot-all-story-urls-20210112170353.csv")
+riot_data["polarity"] = riot_data.apply(f, axis=1)
+
+looting_data = pd.read_csv("looting-all-story-urls-20210117022027.csv")
+looting_data["polarity"] = looting_data.apply(f, axis=1)
+
+mob_data = pd.read_csv("mob-all-story-urls-20210117021905.csv")
+mob_data["polarity"] = mob_data.apply(f, axis=1)
+
+unrest_data = pd.read_csv("unrest-all-story-urls-20210119031600.csv")
+unrest_data["polarity"] = unrest_data.apply(f, axis=1)
+
+floyd_data = pd.read_csv("floyd-all-story-urls-20210119045459.csv")
+floyd_data["polarity"] = floyd_data.apply(f, axis=1)
+
+dc_data = pd.read_csv("capitol-hill-all-story-urls-20210119055556.csv")
+dc_data["polarity"] = dc_data.apply(f, axis=1)
+
+capitol_riot = pd.read_csv("capitol-and-riot-all-story-urls-20210119224550.csv")
+capitol_riot["polarity"] = capitol_riot.apply(f, axis=1)
+
+capitol_protest = pd.read_csv("capitol-and-protest-all-story-urls-20210119224852.csv")
+capitol_protest["polarity"] = capitol_riot.apply(f, axis=1)
+
+blm_data = pd.read_csv("blm-all-story-urls-20210117023329.csv")
+blm_data["polarity"] = blm_data.apply(f, axis=1)
+
+insurrection_data = pd.read_csv("blm-all-story-urls-20210117023329.csv")
+insurrection_data["polarity"] = insurrection_data.apply(f, axis=1)
+
+police_data = pd.read_csv("police-all-story-urls-20210121061536.csv")
+police_data["polarity"] = police_data.apply(f, axis=1)
+
+pb_data = pd.read_csv("proud-boys-all-story-urls-20210123001646.csv")
+pb_data["polarity"] = pb_data.apply(f, axis=1)
+
+antifa_data = pd.read_csv("antifa-all-story-urls-20210123001624.csv")
+antifa_data["polarity"] = antifa_data.apply(f, axis=1)
+
+yemen_data = pd.read_csv("yemen-all-story-urls-20210128185003.csv")
+yemen_data["polarity"] = yemen_data.apply(f, axis=1)
+
+syria_data = pd.read_csv("syria-all-story-urls-20210128185433.csv")
+syria_data["polarity"] = syria_data.apply(f, axis=1)
+
+print(protest_data)
+
+#for index, row in protest_data.iterrows():
+#    media_name = row['media_name'].strip()
+#    try:
+#        print(media_name)
+#        print(media_polarity[row['media_name']])
+#    except:
+#        pass
+
+protest_data.to_csv("protest_2020.csv")
+riot_data.to_csv("riot_2020.csv")
+looting_data.to_csv("looting_2020.csv")
+mob_data.to_csv("mob_2020.csv")
+unrest_data.to_csv("unrest_2020.csv")
+floyd_data.to_csv("floyd_2020.csv")
+dc_data.to_csv("dc_2020.csv")
+blm_data.to_csv("blm_2020.csv")
+capitol_riot.to_csv("capitol_riot_2020.csv")
+capitol_protest.to_csv("capitol_protest_2020.csv")
+insurrection_data.to_csv("insurrection_2020.csv")
+police_data.to_csv("police_2020.csv")
+pb_data.to_csv("pb_2020.csv")
+antifa_data.to_csv("antifa_2020.csv")
+yemen_data.to_csv("yemen_2020.csv")
+syria_data.to_csv("syria_2020.csv")
+
+
+
+
---
+++ protest.R
@@ -0,0 +1,387 @@
+  if (!require(ggplot2)) {
+    install.packages("ggplot2", repos="http://cran.us.r-project.org")
+  }
+  if (!require(wordcloud)) {
+    install.packages("wordcloud", repos="http://cran.us.r-project.org")
+  }
+  library(wordcloud)
+  if (!require(tm)) {
+    install.packages("tm", repos="http://cran.us.r-project.org")
+  }
+  library(tm)
+  if (!require(slam)) {
+    install.packages("slam", repos="http://cran.us.r-project.org")
+  }
+  if (!require(SnowballC)) {
+    install.packages("SnowballC", repos="http://cran.us.r-project.org")
+  }
+  library(SnowballC)
+  if (!require(dplyr)) {
+    install.packages("dplyr", repos="http://cran.us.r-project.org")
+  }
+  library(dplyr)
+  if (!require(tidytext)) {
+    install.packages("tidytext", repos="http://cran.us.r-project.org")
+  }
+  library(tidytext)
+  if (!require(textclean)) {
+    install.packages("textclean", repos="http://cran.us.r-project.org")
+  }
+  library(textclean)
+  if (!require(xts)) {
+    install.packages("xts", repos="http://cran.us.r-project.org")
+  }
+  library(xts)
+  
+  
+  trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+  
+  GenerateWordClouds <- function(dt, data_name, redundant_words) {
+    dt$title = gsub('\'',"",as.character(dt$title))
+    dt$title = gsub('\"',"",as.character(dt$title))
+    dt$title = gsub('\'s',"",as.character(dt$title))
+    
+    #dt_sent = unnest_tokens(dt, "sentences", content, token = "sentences")
+    #subject_words = dt_sent[grep(data_name, dt_sent$sentences),]$sentences
+    subject_words = dt$title
+    subject_words = strip(subject_words, apostrophe.remove=TRUE)
+  
+    words = Corpus(VectorSource(subject_words))
+    corpus <- tm_map(words, content_transformer(tolower))
+    
+    words = tm_map(words, stripWhitespace)
+    words = tm_map(words, tolower)
+    
+    
+    complete_stopwords = c(stopwords('english'), "will", "2020", "campaign", "says", "news", "new", "error", "said", "video")
+    complete_stopwords  = c(complete_stopwords, redundant_words)
+    complete_stopwords = c(complete_stopwords, "fox", "george", "floyd", "cnn", "cnnpolitics", "say", "man", "know", "trumps")
+    # Generate wordcloud removing all stop words
+    png(sprintf("%s_stopwords_wordcloud.png", data_name))
+    words = tm_map(words, removeWords, complete_stopwords)
+    wordcloud(words, max.words = 40, min.freq=5, random.order=FALSE, colors=brewer.pal(8,"Dark2"))
+    
+    dev.off()
+  }
+  
+  get_top_media = function(dt) {
+    dt = dt[order(dt$Freq, decreasing=TRUE),]
+    dt$Var1 = factor(dt$Var1, levels = dt$Var1[order(dt$Freq, decreasing=TRUE)])
+    top_ten = dt[1:10,]
+    return(top_ten)
+  }
+  
+  outlet_bar_plotter = function(top_words, data_name){
+    p = ggplot(top_words, aes(x=Var1, y=Freq)) + geom_bar(stat="identity") 
+    p = p + ggtitle(sprintf("%s - Top Outlets", toupper(data_name)))
+    p = p + theme(plot.title = element_text(hjust = 0.5))
+    p = p + theme(axis.text.x=element_text(angle=90, hjust=1,face="bold"))
+    p = p + theme(axis.text=element_text(size=16,face="bold"), axis.title=element_text(size=13), axis.title.x=element_blank())
+    p = p + theme(plot.title = element_text(size=18,face="bold"))
+    p = p + ylab("Number of Headlines") 
+    ggsave(filename = sprintf("./%s_top.png", data_name) , plot=p, width=4.5, height=6)
+  }
+  
+  polarity_histogram_plotter = function(data, data_name) {
+    data = data[!is.na(data$polarity),]
+    data$polarity = factor(data$polarity)
+    xnames = c('far left', 'left', 'center', 'right', 'far right')
+    p = ggplot(data, aes(x=polarity)) + geom_bar() 
+    p = p + ggtitle(sprintf('Histogram of "%s" Articles', toupper(data_name)))
+    p = p + theme(plot.title = element_text(hjust = 0.5))
+    p = p + theme(axis.text.x=element_text(angle=90, hjust=1,face="bold"))
+    p = p + theme(axis.text=element_text(size=13,face="bold"), axis.title=element_text(size=13), axis.title.x=element_blank())
+    p = p + theme(plot.title = element_text(size=15,face="bold"))
+    p = p + ylab("Number of Articles") 
+    #p = p + xlab("news polarity") 
+    p = p + scale_x_discrete(labels = xnames)
+    ggsave(filename = sprintf("./%s_histogram.png", data_name) , plot=p, width=8, height=6)
+  }
+  # write nice histogram function
+  
+  
+   
+  
+  setwd("/Users/mtauberg/projects/protests/")
+  riot = read.csv("riot_2020.csv")
+  protests = read.csv("protest_2020.csv")
+  looting = read.csv("looting_2020.csv")
+  mob = read.csv("mob_2020.csv")
+  unrest = read.csv("unrest_2020.csv")
+  floyd = read.csv("floyd_2020.csv")
+  dc = read.csv("dc_2020.csv")
+  capitol_riot = read.csv("capitol_riot_2020.csv")
+  capitol_protest = read.csv("capitol_protest_2020.csv")
+  blm = read.csv("blm_2020.csv")
+  insurrection = read.csv("insurrection_2020.csv")
+  police = read.csv("police_2020.csv")
+  pb = read.csv("pb_2020.csv")
+  antifa = read.csv("antifa_2020.csv")
+  yemen = read.csv("yemen_2020.csv")
+  syria = read.csv("syria_2020.csv")
+  # add racism?
+  
+  # use lower case titles for easy grepping
+  riot$title = tolower(riot$title)
+  protests$title = tolower(protests$title)
+  looting$title = tolower(looting$title)
+  unrest$title = tolower(unrest$title)
+  mob$title = tolower(mob$title)
+  floyd$title = tolower(floyd$title)
+  dc$title = tolower(dc$title)
+  blm$title = tolower(blm$title)
+  insurrection$title = tolower(insurrection$title)
+  police$title = tolower(police$title)
+  pb$title = tolower(pb$title)
+  antifa$title = tolower(antifa$title)
+  mob$title = tolower(mob$title)
+  yemen$title = tolower(yemen$title)
+  syria$title = tolower(syria$title)
+  
+  # clean media names
+  riot$media_name = trim(riot$media_name)
+  protests$media_name = trim(protests$media_name)
+  looting$media_name = trim(looting$media_name)
+  unrest$media_name = trim(unrest$media_name)
+  mob$media_name = trim(mob$media_name)
+  floyd$media_name = trim(floyd$media_name)
+  dc$media_name = trim(dc$media_name)
+  blm$media_name = trim(blm$media_name)
+  insurrection$media_name = trim(insurrection$media_name)
+  police$media_name = trim(police$media_name)
+  pb$media_name = trim(pb$media_name)
+  antifa$media_name = trim(antifa$media_name)
+  mob$media_name = trim(mob$media_name)
+  yemen$media_name = trim(yemen$media_name)
+  syria$media_name = trim(syria$media_name)
+  
+  # remove duplicates from everything
+  riot = riot[grep("riot", riot$title),]
+  riot = riot[!duplicated(riot[,c('title')], fromLast=FALSE),]
+  unrest = unrest[grep("unrest", unrest$title),]
+  unrest = unrest[!duplicated(unrest[,c('title')], fromLast=FALSE),]
+  protests = protests[grep("protest", protests$title),]
+  protests = protests[!duplicated(protests[,c('title')], fromLast=FALSE),]
+  floyd = floyd[!duplicated(floyd[,c('title')], fromLast=FALSE),]
+  looting = looting[grep("looting", looting$title),]
+  looting = looting[!duplicated(looting[,c('title')], fromLast=FALSE),]
+  police = police[grep("police", police$title),]
+  police = police[!duplicated(police[,c('title')], fromLast=FALSE),]
+  dc = dc[grep("capitol", dc$title),]
+  dc = dc[!duplicated(dc[,c('title')], fromLast=FALSE),]
+  blm = blm[grep("blm", blm$title),]
+  blm = blm[!duplicated(blm[,c('title')], fromLast=FALSE),]
+  mob = mob[grep("mob", mob$title),]
+  mob = mob[!duplicated(mob[,c('title')], fromLast=FALSE),]
+  yemen = yemen[grep("yemen", yemen$title),]
+  yemen = yemen[!duplicated(yemen[,c('title')], fromLast=FALSE),]
+  syria = syria[grep("syria", syria$title),]
+  syria = syria[!duplicated(syria[,c('title')], fromLast=FALSE),]
+  
+  floyd_riot = floyd[grep("riot", floyd$title), ]
+  floyd_protest = floyd[grep("protest", floyd$title), ]
+  
+  
+  dc_riot = dc[grep("riot", dc$title), ]
+  dc_riot = dc_riot[!duplicated(dc_riot[,c('title')], fromLast=FALSE),]
+  dc_protest = dc[grep("protest", dc$title), ]
+  dc_protest = dc_protest[!duplicated(dc_protest[,c('title')], fromLast=FALSE),]
+  
+  
+  # capitol hill stuff only after 1/6/2020
+  dc$publish_date = substring(dc$publish_date, 1, 19)
+  dc$publish_date = as.Date(dc$publish_date)
+  dc = dc[dc$publish_date > as.Date('2021-01-05'),]  
+  
+  # get list of media entities
+  media_list = trim(protests$media_name)
+  media_list = levels(factor(media_list))
+  
+  
+  
+  # compare to this - https://www.improvethenews.org/
+  
+  
+  # POLARITY OF ARTICLES
+  polarity_histogram_plotter(floyd, "All George Floyd Articles In Sample")
+  polarity_histogram_plotter(floyd_riot, "Floyd + Riot")
+  polarity_histogram_plotter(floyd_protest, "Floyd + Protest")
+  polarity_histogram_plotter(riot, "Riot")
+  polarity_histogram_plotter(protests, "Protest")
+  polarity_histogram_plotter(unrest, "Unrest")
+  polarity_histogram_plotter(looting, "Looting")
+  polarity_histogram_plotter(pb, "Proud Boys")
+  polarity_histogram_plotter(antifa, "Antifa")
+  polarity_histogram_plotter(insurrection, "Insurrection")
+  polarity_histogram_plotter(dc, "Capitol")
+  polarity_histogram_plotter(dc_riot, "Capitol")
+  polarity_histogram_plotter(dc_protest, "Capitol")
+  polarity_histogram_plotter(blm, "BLM")
+  polarity_histogram_plotter(mob, "Mob")
+  polarity_histogram_plotter(yemen, "Yemen")
+  polarity_histogram_plotter(syria, "Syria")
+  
+  # TOP OUTLETS PER TOPIC
+  floyd_media = as.data.frame(table(floyd$media_name))
+  top_floyd_media = get_top_media(floyd_media)
+  outlet_bar_plotter(top_floyd_media, "floyd")
+  
+  police_media = as.data.frame(table(police$media_name))
+  top_police_media = get_top_media(police_media)
+  outlet_bar_plotter(top_police_media, "police")
+  
+  unrest_media = as.data.frame(table(unrest$media_name))
+  top_unrest_media = get_top_media(unrest_media)
+  outlet_bar_plotter(top_unrest_media, "unrest")
+  
+  riot_media = as.data.frame(table(riot$media_name))
+  top_riot_media = get_top_media(riot_media)
+  outlet_bar_plotter(top_riot_media, "riot")
+  
+  protest_media = as.data.frame(table(protests$media_name))
+  top_protest_media = get_top_media(protest_media)
+  outlet_bar_plotter(top_protest_media, "protest")
+  
+  looting_media = as.data.frame(table(looting$media_name))
+  top_looting_media = get_top_media(looting_media)
+  outlet_bar_plotter(top_looting_media, "looting")
+  
+  looting_media = as.data.frame(table(looting$media_name))
+  top_looting_media = get_top_media(looting_media)
+  outlet_bar_plotter(top_looting_media, "looting")
+  
+  capitol_media = as.data.frame(table(dc$media_name))
+  top_capitol_media = get_top_media(capitol_media)
+  outlet_bar_plotter(top_capitol_media, "capitol")
+  
+  pb_media = as.data.frame(table(pb$media_name))
+  top_pb_media = get_top_media(pb_media)
+  outlet_bar_plotter(top_pb_media, "proud boys")
+  
+  antifa_media = as.data.frame(table(antifa$media_name))
+  top_antifa_media = get_top_media(antifa_media)
+  outlet_bar_plotter(top_antifa_media, "antifa")
+  
+  dc_riot_media = as.data.frame(table(dc_riot$media_name))
+  top_dc_riot_media = get_top_media(dc_riot_media)
+  outlet_bar_plotter(top_dc_riot_media, "capitol riot")
+  
+  dc_protest_media = as.data.frame(table(dc_protest$media_name))
+  top_dc_protest_media = get_top_media(dc_protest_media)
+  outlet_bar_plotter(top_dc_protest_media, "capitol protest")
+  
+  insurrection_media = as.data.frame(table(insurrection$media_name))
+  top_insurrection_media = get_top_media(insurrection_media)
+  outlet_bar_plotter(top_insurrection_media, "capitol protest")
+  
+  blm_media = as.data.frame(table(blm$media_name))
+  top_blm_media = get_top_media(blm_media)
+  outlet_bar_plotter(top_blm_media, "capitol protest")
+  
+  mob_media = as.data.frame(table(mob$media_name))
+  top_mob_media = get_top_media(mob_media)
+  outlet_bar_plotter(top_mob_media, "mob")
+  
+  yemen_media = as.data.frame(table(yemen$media_name))
+  top_yemen_media = get_top_media(yemen_media)
+  outlet_bar_plotter(top_yemen_media, "yemen")
+  
+  syria_media = as.data.frame(table(syria$media_name))
+  top_syria_media = get_top_media(syria_media)
+  outlet_bar_plotter(top_syria_media, "syria")
+  
+  
+  mob_media = as.data.frame(table(mob$media_name))
+  top_mob_media = get_top_media(mob_media)
+  outlet_bar_plotter(top_mob_media, "capitol protest")
+  # FOX VS CNN
+  
+  # specific examples of CNN vs FoxNews
+  cnn_protest = protests[protests$media_name == "CNN",]
+  cnn_riot = riot[riot$media_name == "CNN",]
+  cnn_looting = looting[looting$media_name == "CNN",]
+  cnn_blm = blm[blm$media_name == "CNN",]
+  cnn_floyd = floyd[floyd$media_name == "CNN",]
+  cnn_dc = dc[dc$media_name == "CNN",]
+  cnn_unrest = unrest[unrest$media_name == "CNN",]
+  cnn_insurrection = insurrection[insurrection$media_name == "CNN",]
+  cnn_police = police[police$media_name == "CNN",]
+  cnn_pb = pb[pb$media_name == "CNN",]
+  cnn_antifa = antifa[antifa$media_name == "CNN",]
+  cnn_dc = dc[dc$media_name == "CNN",]
+  cnn_mob = mob[mob$media_name == "CNN",]
+  
+  fox_protest = protests[protests$media_name == "Fox News",]
+  fox_riot = riot[riot$media_name == "Fox News",]
+  fox_looting = looting[looting$media_name == "Fox News",]
+  fox_blm = blm[blm$media_name == "Fox News",]
+  fox_floyd = floyd[floyd$media_name == "Fox News",]
+  fox_dc = dc[dc$media_name == "Fox News",]
+  fox_unrest = unrest[unrest$media_name == "Fox News",]
+  fox_insurrection = insurrection[insurrection$media_name == "Fox News",]
+  fox_police = police[police$media_name == "Fox News",]
+  fox_pb = pb[pb$media_name == "Fox News",]
+  fox_antifa = antifa[antifa$media_name == "Fox News",]
+  fox_dc = dc[dc$media_name == "Fox News",]
+  fox_mob = mob[mob$media_name == "Fox News",]
+  
+  
+  remove_words = c("police", "fox", "cnn", "news", "riot", "protest", "protests", "protesters", "riots", "unrest")
+  GenerateWordClouds(fox_riot, "fox_riot", remove_words )
+  GenerateWordClouds(cnn_riot, "cnn_riot", remove_words )
+  GenerateWordClouds(fox_protest, "fox_protest", remove_words )
+  GenerateWordClouds(cnn_protest, "cnn_protest", remove_words )
+  GenerateWordClouds(fox_unrest, "fox_unrest", remove_words )
+  GenerateWordClouds(cnn_unrest, "cnn_unrest", remove_words )
+  
+  remove_words = c("floyd", "floyds", "police", "death")
+  GenerateWordClouds(fox_floyd, "fox_floyd",  remove_words )
+  GenerateWordClouds(cnn_floyd, "cnn_floyd",  remove_words )
+  
+  remove_words = c("police", "officer", "officers", "chief")
+  GenerateWordClouds(fox_police, "fox_police", remove_words )
+  GenerateWordClouds(cnn_police, "cnn_police", remove_words )
+  
+  remove_words = c("looting")
+  GenerateWordClouds(fox_looting, "fox_looting",  remove_words )
+  GenerateWordClouds(cnn_looting, "cnn_looting",  remove_words )
+  
+  remove_words = c("capitol")
+  GenerateWordClouds(fox_dc, "fox_capitol",  remove_words )
+  GenerateWordClouds(cnn_dc, "cnn_capitol",  remove_words )
+  
+  remove_words = c("proud", "boys")
+  GenerateWordClouds(fox_pb, "fox_proud_boys",  remove_words )
+  GenerateWordClouds(cnn_pb, "cnn_proud_boys",  remove_words )
+  
+  remove_words = c("antifa")
+  GenerateWordClouds(fox_antifa, "fox_antifa",  remove_words )
+  GenerateWordClouds(cnn_antifa, "cnn_antifa",  remove_words )
+  
+  remove_words = c("black", "lives", "matter", "blm")
+  GenerateWordClouds(fox_blm, "fox_blm",  remove_words )
+  GenerateWordClouds(cnn_blm, "cnn_blm",  remove_words )
+  
+  remove_words = c("mob")
+  GenerateWordClouds(fox_mob, "fox_mob",  remove_words )
+  GenerateWordClouds(cnn_mob, "cnn_mob",  remove_words )
+  
+  # insurrection as a special case
+  
+  
+  remove_words = c("insurrection")
+  GenerateWordClouds(insurrection, "insurrection_article",  remove_words )
+  
+  
+  insurrection = insurrection[grep("insurrection", insurrection$title),]
+  insurrection = insurrection[!duplicated(insurrection[,c('title')], fromLast=FALSE),]
+  cnn_insurrection = insurrection[insurrection$media_name == "CNN",]
+  fox_insurrection = insurrection[insurrection$media_name == "Fox News",]
+  GenerateWordClouds(insurrection, "insurrection_headline",  remove_words )
+  
+
+  
+  
+  
+  
+  
